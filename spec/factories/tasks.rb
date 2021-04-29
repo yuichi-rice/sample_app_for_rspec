250d4d1cb8dc8558c61bd5c5f8e397cd9c433cea
yuichi-rice <71434566+yuichi-rice@users.noreply.github.com>
2021-04-29 22:57:02 +0900

    自力でできたところ

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task do
-    
+    title {"title"}
+    content {"test_content"}
+    status {:todo}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    #before do
+    #  user_a = FactoryBot.create(:user, email: 'a@example.com', password: 'password', password_confirmation: 'password')
+    #end
+    
+    it 'is valid with all attributes' do
+      task = FactoryBot.build(:task)
+      expect(task).to be_valid
+    end
+
+    it 'is invalid without title' do
+      task = FactoryBot.build(:task, title: nil)
+      task.valid?
+      expect(task.errors[:title]).to include("can't be blank")
+    end
+
+    it 'is invalid without status' do
+      task = FactoryBot.build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank")
+    end
+
+    it 'is invalid with a daplicate title' do
+      FactoryBot.create(:task)
+      task = FctoryBot.create(:task, title: 'test', content: 'testcontent1', status: 0)
+      expect(task.errors[:title]).to include("uniqueness")
+    end
+
+    it 'is valid with another title ' do
+      FactoryBot.create(:task)
+      task = FactoryBot(:task, title: 'test1')
+      expect(task).to be_valid
+    end
+  end
 end
